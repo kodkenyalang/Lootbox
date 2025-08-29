a22980b0d1d20bc220d3551ceac3962fc0b61897
Aaron Ong <aaron.ong@mailfence.com>
2025-08-29 17:52:12 +0800

    Delete contracts directory

--- contracts/IBlocklock.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IBlocklock {
-    function isLocked(uint256 _tokenId) external view returns (bool);
-    function setLock(uint256 _tokenId, uint256 _unlockBlockNumber) external;
-}
